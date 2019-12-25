b99f727553b0d7ae431a0e7a6b04d00c78e92498
Travis CI <travis@travis-ci.org>
2019-12-25 15:25:51 +0800

    update and test,

--- _utils/build_all.sh
+++ _utils/build_all.sh
@@ -5,8 +5,8 @@ docker --version
 # NOTE: for local test only
 # docker kill dell-xps-9560-ubuntu-respin-container
 
-bash ./scripts/docker-build-image.sh
-
-bash ./scripts/get_images.sh
+bash ./_utils/docker-build-image.sh &
+# bash ./_utils/get_images.sh &
+wait
 
 docker run -t --rm --cap-add MKNOD -v $PWD/origin:/docker-input -v $PWD/destination:/docker-output --privileged --name dell-xps-9560-ubuntu-respin-container dell-xps-9560-ubuntu-respin:latest bash build.sh docker-input/ubuntu_1804.iso -c bionicbeaver
