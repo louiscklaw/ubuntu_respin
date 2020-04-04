7c92602a842e50425b899081c004389b467f69ed
louiscklaw <louiscklaw@gmail.com>
2020-04-04 21:11:01 +0800

    update and retest,

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -21,7 +21,7 @@ else
     echo 'skip download image'
 fi
 
-cp ./scripts/build_all.sh ./scripts/isorespin.sh /home/logic/_workspace/ubuntu_respin/origin/
+cp ./scripts/build_all.sh ./scripts/isorespin.sh ./origin/
 
 docker run -t --rm --cap-add MKNOD \
     -v $PWD/origin:/docker-input \
